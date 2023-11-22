7119913b7e20639f242f1d4df8fdc3a47e9a1e66
Rahul Bhatnagar <rb98dps@gmail.com>
2023-11-22 21:24:46 +0530

    initial commit with starter and pom

---
+++ src/main/java/com/devapi/DevApiApplication.java
@@ -0,0 +1,13 @@
+package com.devapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devapi/DevApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
